299fdb40f217b38cc80cd8cd37587fe06c568df6
tracktwo <jcemmett1@gmail.com>
2015-09-05 22:03:01 -0700

    Add front-end so far

---
+++ public/js/barracks.js
@@ -0,0 +1,165 @@
+(function() {
+    var app = angular.module('barracks', []);
+
+    app.controller('CustomSoldierCtrl', ['$scope', function($scope) {
+        this.soldiers = [];
+        this.curSoldier = null;
+
+        this.editSoldier = function(soldier) {
+            if (this.curSoldier == soldier) {
+                this.curSoldier = null;
+            }
+            else {
+                this.curSoldier = soldier;
+            }
+            $scope.$broadcast('soldierChange', this.curSoldier);
+        };
+
+        this.isSelected = function(soldier) {
+            return this.curSoldier === soldier;
+        };
+
+        this.addSoldier = function(soldier) {
+            this.soldiers.push(angular.copy(soldier));
+        };
+
+        this.saveChanges = function(soldier) {
+            angular.copy(soldier, this.curSoldier);
+            this.curSoldier = null;
+        };
+
+        this.printName = function(name) {
+            if (name == '') {
+                return "<random>";
+            }
+            else {
+                return name;
+            }
+        };
+    }]);
+
+    app.controller('SoldierForm', ['$scope', function($scope) {
+
+        this.soldier = {};
+        this.curSoldier = null;
+        this.extraPerkStr = '';
+
+        var frm = this;
+
+        this.resetSoldier = function () {
+            this.soldier.firstName = "";
+            this.soldier.lastName = "";
+            this.soldier.nickName = "";
+            this.soldier.rank = $scope.Ranks[0];
+            this.soldier.class = $scope.Classes[0];
+            this.soldier.gender = $scope.Genders[0];
+            this.soldier.psiRank = $scope.PsiRanks[0];
+            this.soldier.country = "Unspecified";
+            this.soldier.classPerks = ["", "", "", "", "", "" ];
+            this.soldier.psiPerks = ["", "", "", "", "", "" ];
+            this.soldier.extraPerks = [];
+        };
+
+        this.resetSoldier();
+
+        this.submitSoldier = function (soldierCtrl) {
+            if (this.isEditing()) {
+                soldierCtrl.saveChanges(this.soldier);
+            }
+            else {
+                soldierCtrl.addSoldier(this.soldier);
+            }
+            this.resetSoldier();
+            this.curSoldier = null;
+        };
+
+        this.rankChanged = function () {
+            if (this.soldier.rank == $scope.Ranks[0]) {
+                this.soldier.class = $scope.Classes[0];
+            }
+
+            var rankIdx = $scope.Ranks.indexOf(this.soldier.rank);
+            for (i = 0; i < 6; ++i) {
+                if (rankIdx < (i + 2)) {
+                    this.soldier.classPerks[i] = "";
+                }
+
+            }
+        };
+
+        this.classChanged = function () {
+            // Invalidate all the class perks
+            for (i = 0; i < 6; ++i)
+            {
+                this.soldier.classPerks[i] = "";
+            }
+        };
+
+        this.isClassDisabled = function() {
+            return this.soldier.rank == $scope.Ranks[0];
+        };
+
+        $scope.$on('soldierChange', function(event, s) {
+            if (s == null) {
+                frm.resetSoldier();
+            }
+            else {
+                angular.copy(s, frm.soldier);
+            }
+            frm.curSoldier = s;
+            $scope.rankChanged();
+        });
+
+        this.isEditing = function() {
+            return this.curSoldier != null;
+        };
+
+        this.submitLabel = function() {
+            return this.isEditing() ? "Save Changes" : "Add Soldier";
+        };
+
+        this.isPerkDisabled = function(rank) {
+            return this.soldier.class == "None" ||
+                ($scope.Ranks.indexOf(this.soldier.rank) < $scope.Ranks.indexOf(rank));
+        };
+
+        this.isPsiPerkDisabled = function(rank) {
+            return this.soldier.psiRank == "None" ||
+                ($scope.PsiRanks.indexOf(this.soldier.psiRank) < $scope.PsiRanks.indexOf(rank));
+        };
+
+        this.addExtraPerk = function() {
+            this.soldier.extraPerks.push(this.extraPerkStr);
+            this.extraPerkStr = '';
+        };
+
+        this.removeExtraPerk = function(idx) {
+           // var idx = this.soldier.extraPerks.indexOf(perk);
+            this.soldier.extraPerks.splice(idx, 1);
+        }
+
+        this.isExtraPerkInvalid = function() {
+            return this.extraPerkStr == "" ||
+                this.soldier.extraPerks.indexOf(this.extraPerkStr) != -1;
+        }
+    }]);
+
+    app.directive('perkBox', function() {
+       return {
+           restrict: 'E',
+           scope: {
+               rank: "@",
+               labelWidth: "@",
+               isDisabled: "=",
+               ngModel: "="
+           },
+           transclude: true,
+           template: '<div class="form-group">' +
+           '<label class="control-label col-xs-{{labelWidth}}">{{rank}}</label>' +
+          // '</div>' +
+           '<div class="col-xs-{{12-labelWidth}}"><select class="form-control" ng-disabled="isDisabled" ng-model="ngModel" ng-transclude></select></div>' +
+           '</div>'
+       };
+    });
+
+})();
---
+++ public/js/jumpstarter.js
@@ -0,0 +1,90 @@
+(function() {
+    var app = angular.module('jumpstarter', ['ui.bootstrap', 'tabs', 'barracks']);
+
+    app.controller('JumpStarterCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.Ranks = [
+            "PFC",
+            "SPEC",
+            "LCPL",
+            "CPL",
+            "SGT",
+            "TSGT",
+            "GSGT",
+            "MSGT"
+        ];
+
+        $scope.Classes = [
+            "None",
+            "Sniper",
+            "Scout",
+            "Assault",
+            "Infantry",
+            "Rocketeer",
+            "Gunner",
+            "Medic",
+            "Engineer"
+        ];
+
+        $scope.Genders = [
+            "",
+            "M",
+            "F"
+        ];
+
+        $scope.Countries = [
+            "Unspecified"
+        ];
+
+        $scope.PsiRanks = [
+            "None",
+            "Awakened",
+            "Sensitive",
+            "Talent",
+            "Adept",
+            "Psion",
+            "Master"
+        ];
+
+        $scope.getPerkName = function(perkEnum) {
+            return $scope.Perks.filter(function(v) {
+                return v.enum === perkEnum;
+            })[0].name;
+        };
+
+        $http.get("data/countries.json").then( function (response) {
+            $scope.Countries = response.data.Countries;
+        });
+
+        $http.get("data/class-perks.json").then(function (response) {
+            $scope.ClassPerks = response.data;
+        });
+
+        $http.get("data/perks.json").then(function (response) {
+            $scope.Perks = response.data;
+        });
+
+        $http.get("data/psi-perks.json").then(function (response) {
+            $scope.PsiPerks = response.data;
+        });
+    }]);
+
+    // Workaround for triggering change events on keyboard use with select elements.
+    app.directive("select", function() {
+        return {
+            restrict: "E",
+            require: "?ngModel",
+            scope: false,
+            link: function (scope, element, attrs, ngModel) {
+                if (!ngModel) {
+                    return;
+                }
+                element.bind("keyup", function() {
+                    element.triggerHandler("change");
+                })
+            }
+        }
+    })
+
+})();
+
+
