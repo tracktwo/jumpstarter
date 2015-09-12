b3f6668cb5950e0dc32df5f8a0c2f59cfc087f22
tracktwo <jcemmett1@gmail.com>
2015-09-11 22:21:44 -0700

    Moved soldier config to a modal dialog

--- public/js/barracks.js
+++ public/js/barracks.js
@@ -1,177 +1,164 @@
-(function() {
-    var app = angular.module('barracks', []);
+(function () {
+  var app = angular.module('barracks', ['ui.bootstrap']);
 
-    app.controller('CustomSoldierCtrl', ['$scope', function($scope) {
-        this.curSoldier = null;
+  app.controller('CustomSoldierCtrl', ['$scope', '$modal', function ($scope, $modal) {
+    var ctrl = this;
 
-        this.editSoldier = function(soldier) {
-            if (this.curSoldier == soldier) {
-                this.curSoldier = null;
-            }
-            else {
-                this.curSoldier = soldier;
-            }
-            $scope.$broadcast('soldierChange', this.curSoldier);
-        };
+    this.editSoldier = function (soldier) {
+      this.openSoldierDialog(soldier);
+    };
 
-        this.isSelected = function(soldier) {
-            return this.curSoldier === soldier;
-        };
+    this.newSoldier = function() {
+      this.openSoldierDialog(null);
+    }
 
-        this.addSoldier = function(soldier) {
-            $scope.ini.soldiers.push(angular.copy(soldier));
-        };
 
-        this.saveChanges = function(soldier) {
-            angular.copy(soldier, this.curSoldier);
-            this.curSoldier = null;
-        };
+    this.openSoldierDialog = function(curSoldier) {
+      var newSoldierDlg = $modal.open({
+        scope: $scope,
+        size: 'lg',
+        templateUrl: 'barracks-soldier-form.html',
+        controller: 'SoldierForm',
+        controllerAs: 'frm',
+        resolve: {
+          curSoldier: function() {
+            return curSoldier;
+          }
+        }
+      });
 
-        this.printName = function(name) {
-            if (name == '') {
-                return "<random>";
-            }
-            else {
-                return name;
-            }
-        };
-    }]);
+      newSoldierDlg.result.then(function (resultSoldier) {
+        if (curSoldier == null) {
+          $scope.ini.soldiers.push(resultSoldier);
+        } else {
+          angular.copy(resultSoldier, curSoldier);
+        }
+      });
+    };
 
-    app.controller('SoldierForm', ['$scope', function($scope) {
+    this.delSoldier = function(idx) {
+      $scope.ini.soldiers.splice(idx, 1);
+    };
 
-        this.soldier = {};
-        this.curSoldier = null;
+    this.printName = function (name) {
+      if (name == '') {
+        return "<random>";
+      }
+      else {
+        return name;
+      }
+    };
+  }]);
 
-        var frm = this;
+  app.controller('SoldierForm', function ($scope, $modalInstance, curSoldier) {
 
-        this.resetSoldier = function () {
-            this.soldier.firstName = "";
-            this.soldier.lastName = "";
-            this.soldier.nickName = "";
-            this.soldier.rank = $scope.Ranks[0];
-            this.soldier.class = $scope.Classes[0];
-            this.soldier.gender = $scope.Genders[0];
-            this.soldier.psiRank = $scope.PsiRanks[0];
-            this.soldier.country = "";
-            this.soldier.classPerks = ["", "", "", "", "", "" ];
-            this.soldier.psiPerks = ["", "", "", "", "", "" ];
-            this.soldier.extraPerks = [];
-            this.soldier.hp = 0;
-            this.soldier.aim = 0;
-            this.soldier.will = 0;
-            this.soldier.mob = 0;
-            this.soldier.bonusattrib = true;
-        };
+    this.soldier = {};
 
-        this.resetSoldier();
+    if (curSoldier == null) {
+      this.soldier = {
+        firstName: "",
+        lastName: "",
+        nickName: "",
+        rank: $scope.Ranks[0],
+        class: $scope.Classes[0],
+        gender: $scope.Genders[0],
+        psiRank: $scope.PsiRanks[0],
+        country: "",
+        classPerks: ["", "", "", "", "", ""],
+        psiPerks: ["", "", "", "", "", ""],
+        extraPerks: [],
+        hp: 0,
+        aim: 0,
+        will: 0,
+        mob: 0,
+        defense: 0,
+        bonusAttrib: true
+      };
+    }
+    else {
+      angular.copy(curSoldier, this.soldier);
+      this.curSoldier = curSoldier;
+    }
 
-        this.submitSoldier = function (soldierCtrl) {
-            if (this.isEditing()) {
-                soldierCtrl.saveChanges(this.soldier);
-            }
-            else {
-                soldierCtrl.addSoldier(this.soldier);
-            }
-            this.resetSoldier();
-            this.curSoldier = null;
-        };
 
-        this.rankChanged = function () {
-            if (this.soldier.rank == $scope.Ranks[0]) {
-                this.soldier.class = $scope.Classes[0];
-            }
+    var frm = this;
 
-            var rankIdx = $scope.Ranks.indexOf(this.soldier.rank);
-            for (i = 0; i < 6; ++i) {
-                if (rankIdx < (i + 2)) {
-                    this.soldier.classPerks[i] = "";
-                }
+    this.submitSoldier = function () {
+      $modalInstance.close(this.soldier);
+    };
 
-            }
-        };
+    this.cancel = function() {
+      $modalInstance.dismiss('cancel');
+    };
 
-        this.classChanged = function () {
-            // Invalidate all the class perks
-            for (i = 0; i < 6; ++i)
-            {
-                this.soldier.classPerks[i] = "";
-            }
-        };
+    this.rankChanged = function () {
+      if (this.soldier.rank == $scope.Ranks[0]) {
+        this.soldier.class = $scope.Classes[0];
+      }
 
-        this.isClassDisabled = function() {
-            return this.soldier.rank == $scope.Ranks[0];
-        };
+      var rankIdx = $scope.Ranks.indexOf(this.soldier.rank);
+      for (i = 0; i < 6; ++i) {
+        if (rankIdx < (i + 2)) {
+          this.soldier.classPerks[i] = "";
+        }
 
-        $scope.$on('soldierChange', function(event, s) {
-            if (s == null) {
-                frm.resetSoldier();
-            }
-            else {
-                angular.copy(s, frm.soldier);
-            }
-            frm.curSoldier = s;
-            frm.rankChanged();
-        });
+      }
+    };
 
-        this.isEditing = function() {
-            return this.curSoldier != null;
-        };
+    this.classChanged = function () {
+      // Invalidate all the class perks
+      for (i = 0; i < 6; ++i) {
+        this.soldier.classPerks[i] = "";
+      }
+    };
 
-        this.submitLabel = function() {
-            return this.isEditing() ? "Save Changes" : "Add Soldier";
-        };
+    this.isClassDisabled = function () {
+      return this.soldier.rank == $scope.Ranks[0];
+    };
 
-        this.isPerkDisabled = function(rank) {
-            return this.soldier.class == "None" ||
-                ($scope.Ranks.indexOf(this.soldier.rank) < $scope.Ranks.indexOf(rank));
-        };
+    this.isPerkDisabled = function (rank) {
+      return this.soldier.class == "None" ||
+        ($scope.Ranks.indexOf(this.soldier.rank) < $scope.Ranks.indexOf(rank));
+    };
 
-        this.isPsiPerkDisabled = function(rank) {
-            return this.soldier.psiRank == "None" ||
-                ($scope.PsiRanks.indexOf(this.soldier.psiRank) < $scope.PsiRanks.indexOf(rank));
-        };
+    this.isPsiPerkDisabled = function (rank) {
+      return this.soldier.psiRank == "None" ||
+        ($scope.PsiRanks.indexOf(this.soldier.psiRank) < $scope.PsiRanks.indexOf(rank));
+    };
 
-        this.addExtraPerk = function(prk) {
-            this.soldier.extraPerks.push(prk);
-        };
+    this.addExtraPerk = function (prk) {
+      this.soldier.extraPerks.push(prk);
+    };
 
-        this.removeExtraPerk = function(idx) {
-           // var idx = this.soldier.extraPerks.indexOf(perk);
-            this.soldier.extraPerks.splice(idx, 1);
-        };
+    this.removeExtraPerk = function (idx) {
+      // var idx = this.soldier.extraPerks.indexOf(perk);
+      this.soldier.extraPerks.splice(idx, 1);
+    };
 
-        this.isExtraPerkInvalid = function(prk) {
-            return prk == "" ||
-                this.soldier.extraPerks.indexOf(prk) != -1;
-        };
+    this.isExtraPerkInvalid = function (prk) {
+      return prk == "" ||
+        this.soldier.extraPerks.indexOf(prk) != -1;
+    };
+  });
 
-        this.resetAttributes = function() {
-            this.soldier.hp = 0;
-            this.soldier.aim = 0;
-            this.soldier.mob = 0;
-            this.soldier.will = 0;
-            this.soldier.bonusattrib = true;
-        };
-    }]);
-
-    app.directive('perkBox', function() {
-       return {
-           restrict: 'E',
-           scope: {
-               rank: "@",
-               labelWidth: "@",
-               isDisabled: "=",
-               ngModel: "=",
-               options: "=",
-               render: "&"
-           },
-           transclude: true,
-           template: '<div class="form-group">' +
-           '<label class="control-label col-xs-{{labelWidth}}">{{rank}}</label>' +
-          // '</div>' +
-           '<div class="col-xs-{{12-labelWidth}}"><select class="form-control" ng-disabled="isDisabled" ng-model="ngModel" ng-options="render({opt: o}) for o in options"></select></div>' +
-           '</div>'
-       };
-    });
+  app.directive('perkBox', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        rank: "@",
+        labelWidth: "@",
+        isDisabled: "=",
+        ngModel: "=",
+        options: "=",
+        render: "&"
+      },
+      transclude: true,
+      template: '<div class="form-group">' +
+      '<label class="control-label col-xs-{{labelWidth}}">{{rank}}</label>' +
+        // '</div>' +
+      '<div class="col-xs-{{12-labelWidth}}"><select class="form-control" ng-disabled="isDisabled" ng-model="ngModel" ng-options="render({opt: o}) for o in options"></select></div>' +
+      '</div>'
+    };
+  });
 
 })();
